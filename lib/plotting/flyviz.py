db4261f86757fbaad13e0cd85cd075b60e736a8e
lkrower <lkrower2000@gmail.com>
2024-04-26 13:56:41 -0700

    new stuff

--- lib/plotting/flyviz.py
+++ lib/plotting/flyviz.py
@@ -15,6 +15,7 @@ def get_objectmask(maskFly,max_contour):
     gen_mask = np.zeros(maskFly.shape,dtype='uint8')
     # convert image to BGR
     img_fly_mask_cont = cv2.cvtColor(gen_mask,cv2.COLOR_GRAY2BGR)
+    
     # draw the contours on this mask
     cv2.drawContours(img_fly_mask_cont,[max_contour],contourIdx =0, color = (255,255,255),thickness = 4)
     return img_fly_mask_cont
@@ -30,6 +31,180 @@ def get_fill_flymask(maskFly, max_contour):
     # fill in the fly contour to get the mask
     cv2.fillPoly(cont_mask,[max_contour],color = (255))
     return cont_mask
+
+def get_contour_centroid(img):
+    """
+    This function takes an input of an image and returns the contour, centroid and centerline
+
+    """
+    img_dict = {
+        'img':None,
+        'centroid': None,
+        'body_axis_pt_0':None,
+        'body_axis_pt_1':None
+    }
+    # read in the image file of the fly as Grayscale
+    img_fly = cv2.imread(img,cv2.IMREAD_GRAYSCALE)
+    # Get basic image data
+    height, width = img_fly.shape
+    print("height",height)
+    image_cvsize = width, height 
+    mid_x, mid_y = 0.5*width, 0.5*height
+    # perform otsu thresholding
+    ### set fly_mask to be img_otsu if wanting to add in a morphological operation
+    rval, fly_mask= cv2.threshold(img_fly,25,np.iinfo(img_fly.dtype).max,cv2.THRESH_BINARY_INV)
+
+    # (Optional) -> Morphology: erosion
+    strel = np.ones((5,5),np.uint8)  
+
+    # maskFly = cv2.dilate(img_otsu, strel)
+    # perform an erosing
+    ## MORPHOLOCICAL OPERATION IN ORDER TO INCORPORATE SMOOTHING!
+    # fly_mask = cv2.morphologyEx( cv2.morphologyEx(img_otsu, cv2.MORPH_CLOSE, strel), cv2.MORPH_OPEN, strel)
+
+
+    # get the contours
+    contours,hierarchy = cv2.findContours(fly_mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    # utilize find_fly_angle function to get max area and contour
+    max_contour, max_area = get_max_area_contour(contours)
+    # determine the centroid using find_fly_angle function
+    Cx, Cy = get_centroid(cv2.moments(max_contour))# centroid x,y coordinates
+    ## centroid
+    centroid = (int(Cx), int(Cy))
+    # determine the angle and body vectory using finde_fly_angle function
+    angle, body_vect = get_angle_and_body_vector(cv2.moments(max_contour))
+    # Get bounding box and find diagonal - used for drawing body axis
+    bbox = cv2.boundingRect(max_contour)
+    bbox_diag = np.sqrt(bbox[2]**2 + bbox[3]**2)
+    # Create points for drawing axis fly in contours image 
+    axis_length = 0.75*bbox_diag
+    body_axis_pt_0 = int(Cx+ axis_length*body_vect[0]), int(Cy+ axis_length*body_vect[1])
+    body_axis_pt_1 = int(Cx- axis_length*body_vect[0]), int(Cy - axis_length*body_vect[1])
+    print(body_axis_pt_0)
+    return fly_mask, max_contour,centroid, body_axis_pt_0,body_axis_pt_1
+
+
+    # # Compute cirlce mask
+    # mask_radius = int(.95*height/2.0)
+    # print('mask radius',mask_radius)
+    # vals_x = np.arange(0.0,width)
+    # vals_y = np.arange(0.0,height)
+    # grid_x, grid_y = np.meshgrid(vals_x, vals_y)    ## plot the centroid
+    # # plt.plot(centroid[0],centroid[1],marker = '.',color = 'r' ,markersize= 10)
+
+    # ## plot arrow of direction
+    # endpoint_x = centroid[0] + axis_length*body_vect[0]
+    # endpoint_y = centroid[1] - axis_length*body_vect[1]
+    # # plmalet.arrow(body_axis_pt_0[0],body_axis_pt_0[1],body_axis_pt_1[0],body_axis_pt_1[1],
+    # #             width=1,  # Width of the arrow
+    # #             head_width=30,  # Width of the arrow head
+    # #             head_length=35,  # Length of the arrow head
+    # #             fc='blue',  # Fill color of the arrow
+    # #             ec='blue',  # Edge color of the arrow
+    # #             linestyle='-',  # Style of the arrow
+    # #             length_includes_head=True
+
+    # #             )
+    # # cv2.line(img_fly, body_axis_pt_0, body_axis_pt_1, (0,0,255), 2)
+
+    # # ##  plot the image
+    # # plt.imshow(img_fly,cmap ='gray')
+    
+    # circ_mask = (grid_x - width/2.0 + 0.5)**2 + (grid_y - height/2.0 + 0.5)**2 < (mask_radius)**2
+
+
+
+    # # Get matrices for shifting (centering) and rotating the image
+    # shift_mat = np.matrix([[1.0, 0.0, (mid_x - Cx)], [0.0, 1.0, (mid_y - Cy)]]) 
+    # rot_mat = cv2.getRotationMatrix2D((mid_x, mid_y),np.rad2deg(angle),1.0)
+
+    # # Shift and rotate the original image
+    # shifted_image = cv2.warpAffine(img_fly, shift_mat, image_cvsize)
+    # rotated_image = cv2.warpAffine(shifted_image,rot_mat,image_cvsize)
+
+    # # Shift and rotate threshold image. 
+    # shifted_threshold_image = cv2.warpAffine(fly_mask, shift_mat, image_cvsize)
+    # rotated_threshold_image = cv2.warpAffine(shifted_threshold_image,rot_mat,image_cvsize)
+    # rotated_threshold_image = rotated_threshold_image*circ_mask
+    # rval, rotated_threshold_image  = cv2.threshold(rotated_threshold_image,0,256,cv2.THRESH_OTSU +cv2.THRESH_BINARY_INV)
+    # # Get orientation discriminant and flip image if needed 
+    # orient_ok, orient_discrim= is_orientation_ok(rotated_threshold_image,2)
+    # if not orient_ok:
+    #     rot_180_mat = cv2.getRotationMatrix2D((mid_x, mid_y),-180.0,1.0)
+    #     rotated_image = cv2.warpAffine(rotated_image,rot_180_mat,image_cvsize)
+    #     rotated_threhold_image = cv2.warpAffine(rotated_threshold_image,rot_180_mat,image_cvsize)
+    #     angle += np.deg2rad(-180.0)
+
+    # # angle = normalize_angle_range(angle)
+    # # print("angle final", angle)        fly_mask = cv2.morphologyEx( cv2.morphologyEx(img_otsu, cv2.MORPH_CLOSE, strel), cv2.MORPH_OPEN, strel)
+
+    # # The images will be kept in grayscale
+    # # Correct contours
+    # max_cont_fix = max_contour.reshape(-1,2)
+    # ## plot the contour
+    # # plt.plot(max_cont_fix[:,0],max_cont_fix[:,1],color = 'r', linewidth = 2)
+
+    # # Here we will save the information to the corresponding dictionary key
+    # fly_gender = type_list[idx]
+    # img_dict[fly_gender]['img'] = img_fly
+    # img_dict[fly_gender]['centroid'] = centroid
+    # img_dict[fly_gender]['body_axis_pt_0']= body_axis_pt_0
+    # img_dict[fly_gender]['body_axis_pt_1']= body_axis_pt_1
+    # img_dict[fly_gender]['max contour']= max_cont_fix
+
+
+
+
+# if display == True:
+#     # print(max_cont_fix)
+#     plt.figure(figsize=(24,24))
+#     ## will create 4 image panels
+#     ### 1 the original male image
+#     plt.subplot(2,2,1)
+#     plt.title(f"Original {fly1} Image", fontsize = 20)
+#     plt.axis('off')
+#     plt.imshow(img_dict['male']['img'],cmap = 'gray')
+#     # ### 2 optional show the image mask
+#     # plt.subplot(2,2,2)
+#     # plt.tiangletle("Fly Mask", fontsize = 10)
+#     # masked_filled_fly = get_fill_flymask(fly_mask, max_contour)
+#     # plt.imshow(masked_filled_fly,cmap ='gray')
+#     ### 3 original male image with the contour and line    ## plot the centroid
+#     plt.subplot(2,2,2)
+#     plt.title(f"{fly1} Fly Contour and Orientation", fontsize = 20)
+    
+#     plt.plot(img_dict['male']['centroid'][0],img_dict['male']['centroid'][1],marker = '.',color = 'r' ,markersize= 10)
+#     ## plot the contour
+#     plt.plot(img_dict['male']['max contour'][:,0],img_dict['male']['max contour'][:,1],color = 'r', linewidth = 2)
+#     # cv2.line(img_fly, body_axis_pt_0, body_axis_pt_1, (0,0,255), 2)
+#     print(img_dict['male']['body_axis_pt_0'])
+#     plt.plot((img_dict['male']['body_axis_pt_0'][0],img_dict['male']['body_axis_pt_1'][0]),(img_dict['male']['body_axis_pt_0'][1],img_dict['male']['body_axis_pt_1'][1]), color = 'r', linewidth =2)
+#     # cv2.circle(img_fly, (int(centroid[0] + np.cos(angle)*mask_radius),int(centroid[1] + np.sin(angle)*mask_radius)), 10, (0,255,255), -1)
+#     plt.axis('off')
+#     plt.imshow(img_dict['male']['img'],cmap = 'gray')
+    
+#     ### 3 the original female image
+#     plt.subplot(2,2,3)
+#     plt.title(f"Original {fly2} Image", fontsize = 20)
+#     plt.axis('off')
+#     plt.imshow(img_dict['female']['img'],cmap = 'gray')
+#     # ### 2 optional show the image mask
+#     # plt.subplot(2,2,2)
+#     # plt.tiangletle("Fly Mask", fontsize = 10)
+#     # masked_filled_fly = get_fill_flymask(fly_mask, max_contour)
+#     # plt.imshow(masked_filled_fly,cmap ='gray')
+#     ### 4 original female image with the contour and line    ## plot the centroid
+#     plt.subplot(2,2,4)
+#     plt.title(f"{fly2} Fly Contour and Orientation", fontsize = 20)
+#     ## plot the contour
+#     plt.plot(img_dict['female']['max contour'][:,0],img_dict['female']['max contour'][:,1],color = 'r', linewidth = 2)
+#     ## plot the centerline
+#     plt.plot((img_dict['female']['body_axis_pt_0'][0],img_dict['female']['body_axis_pt_1'][0]),(img_dict['female']['body_axis_pt_0'][1],img_dict['female']['body_axis_pt_1'][1]), color = 'r', linewidth =2)
+#     ## get rid of the axis
+#     plt.axis('off')
+#     ##  plot the image
+#     plt.imshow(img_fly,cmap ='gray')
+#     plt.savefig('figs/2by2_orig_thr.svg',facecolor = 'white')
 
 ## single image
 def single_image_save(file_name,display=False):
