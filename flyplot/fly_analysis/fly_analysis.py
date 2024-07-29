9d24d84040026a744557fbcfb895a1b937a27482
loganrower <lkrower2000@gmail.com>
2024-07-29 12:07:38 -0700

    testing ... issues with flyflip copy

--- flyplot/fly_analysis/fly_analysis.py
+++ flyplot/fly_analysis/fly_analysis.py
@@ -19,52 +19,88 @@ import os
 import shutil
 import json
 import sys
+import configparser
 
-# Set Directory Path for Text file data and the Notebook copy
-print("Set Location for Fly Flip Notebook Copy") # same as textfile location
-path_dir = "/media/loganrower/D5E2-7968/20240502125110_data/"#input("Target Directory: ")
+def flyplot_setup():
+    """
+    generates config file within current directory to use for data paths
+    """
+    config = configparser.ConfigParser()
+    
+    print("\nEstablish Image Directory Path:")
+    imgs_dir = "'"+input("")+"'" # "'/home/flyranch/image_data/20240621/fly1/20240621102556'"
+    date_exp = imgs_dir.strip("'").split('/')[-1]
+    print("Set Location for Fly Flip Notebook Copy") # same as textfile location
+    path_dir = input("Target Directory: ") # "/media/loganrower/D5E2-7968/20240502125110_data/" 
+    path_dir_dict = "'"+path_dir+"'"
+    print("Textfile Data Path:")
+    txtfile = "'"+path_dir+"/"+date_exp+".txt"+"'" # "'/media/loganrower/D5E2-7968/20240502125110_data/20240502125110.txt'" 
 
-# Set Image Directory 
-print("\nEstablish Image Directory Path:")
-imgs_dir = "'"+input("")+"'" # "'/home/flyranch/image_data/20240621/fly1/20240621102556'" 
-date_exp = imgs_dir.strip("'").split('/')[-1]
-print("Experiment Date: ",date_exp,"\n")
+    config['Data Paths'] ={'image_data':imgs_dir,
+                           'raw_data_dir':path_dir_dict,
+                           'txtfile':txtfile }
+    # Write the configuration to a file
+    print(path_dir.strip("'"))
+    config_path = os.path.join(path_dir.strip("'"),'config.ini')
+    with open(config_path, 'w') as configfile:
+	    config.write(configfile)
+    
+def flyplot_copy():
+    """
+    Generates notebook copy of template using the config.ini that was set up 
+    to populate the directory paths. This allows the user to create multiple different 
+    jupyter notebooks based on their analysis. 
 
-# Set Text file
-print("Textfile Data Path:")
-txtfile = "'"+path_dir+input("")+"'" # "'/media/loganrower/D5E2-7968/20240502125110_data/20240502125110.txt'" 
-date_exp = imgs_dir.strip("'").split('/')[-1]
+    """
+    # Set Directory Path for Text file data and the Notebook copy
+    print("Set Location for Fly Flip Notebook Copy") # same as textfile location
+    path_dir = input("Target Directory: ") # "/media/loganrower/D5E2-7968/20240502125110_data"
+    # Set Image Directory 
+    print("\nEstablish Image Directory Path:")
+    imgs_dir = "'"+input("")+"'" # "'/home/flyranch/image_data/20240621/fly1/20240621102556'" 
+    date_exp = imgs_dir.strip("'").split('/')[-1]
+    print("Experiment Date: ",date_exp,"\n")
 
-# Copy Notebook
-print("Copied Notebook Location")
-target_dir = f'{path_dir}/flyflip_{date_exp}.ipynb'
-notebook_loc = shutil.copyfile('flyflip.ipynb',target_dir)
-print(notebook_loc,"\n")
+    # Set Text file
+    print("Textfile Name:")
+    txtfile = "'"+path_dir+input("")+"'" # "'/media/loganrower/D5E2-7968/20240502125110_data/20240502125110.txt'" 
+    date_exp = imgs_dir.strip("'").split('/')[-1]
 
-img_source_new = ""
-txt_source_new =""
-fly_book_content ={}
+    print("EXP Date",date_exp)
+    # Copy Notebook
+    print("Copied Notebook Location")
+    target_dir = f'{path_dir}/flyflip_{date_exp}.ipynb'
+    print("Does notebook exists?",os.path.exists('flyflip.ipynb'))
+    print("Does target path exists?",os.path.exists(path_dir))
+    notebook_loc = shutil.copyfile('flyflip.ipynb',target_dir)
+    print(notebook_loc,"\n")
 
-# read data and add to dictionary
-with open(target_dir, 'r', encoding='utf-8') as fly_book:
-    fly_book_content = json.load(fly_book)
-    img_source = fly_book_content['cells'][6]['source'][0].split("=")
-    print(img_source)
-    img_source_new = img_source[0] + " = " + str(imgs_dir) 
-    print(img_source_new)
-    txt_source = fly_book_content['cells'][7]['source'][0].split("=")
-    print(txt_source)
-    txt_source_new = txt_source[0] + " = " + str(txtfile) 
-    fly_book_content['cells'][7]['source'][0] = txt_source_new
+    img_source_new = ""
+    txt_source_new =""
+    fly_book_content ={}
 
-# Write paths
-with open(target_dir, 'w', encoding='utf-8') as fly_book:
-    json.dump(fly_book_content,fly_book)
+    # read data and add to dictionary
+    with open(target_dir, 'r', encoding='utf-8') as fly_book:
+        fly_book_content = json.load(fly_book)
+        img_source = fly_book_content['cells'][6]['source'][0].split("=")
+        print(img_source)
+        img_source_new = img_source[0] + " = " + str(imgs_dir) 
+        print(img_source_new)
+        txt_source = fly_book_content['cells'][7]['source'][0].split("=")
+        print(txt_source)
+        txt_source_new = txt_source[0] + " = " + str(txtfile) 
+        fly_book_content['cells'][7]['source'][0] = txt_source_new
 
-# Check the changed paths
-with open(target_dir, 'r', encoding='utf-8') as fly_book:
-    fly_book_content = json.load(fly_book)
-    source_img = fly_book_content['cells'][6]['source'][0].split("=")
-    print(source_img)
-    source_txt = fly_book_content['cells'][7]['source'][0].split("=")
-    print(source_txt)
+    # Write paths
+    with open(target_dir, 'w', encoding='utf-8') as fly_book:
+        json.dump(fly_book_content,fly_book)
+
+    # Check the changed paths
+    with open(target_dir, 'r', encoding='utf-8') as fly_book:
+        fly_book_content = json.load(fly_book)
+        source_img = fly_book_content['cells'][6]['source'][0].split("=")
+        print(source_img)
+        source_txt = fly_book_content['cells'][7]['source'][0].split("=")
+        print(source_txt)
+if __name__ == "__main__":
+     flyplot_setup()
--- flyplot/parsing/fly_wrap.py
+++ flyplot/parsing/fly_wrap.py
@@ -1,12 +1,73 @@
 """
 Methods for dealing with fruit fly wrapping
 """
-from lib.plotting import polarplot as polarplt
+from flyplot.plotting import polarplot as polarplt
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 import numpy as np
 
-def wrapping_fix(data,subgroups,plot=True):
+def wrapping_fix(df_orig,df_revised):
+    """
+    Parameters:
+        - df_orig:
+            Corresponds to the dataframe `df_fly_org`, or any other dataframe that contains 
+            the original data from Magnotether experiment.
+        
+        - df_revised:
+            Corresponds to the dataframe `df_fly_revised`, or any other dataframe taht contains 
+            revised Magnotether data after going through MSE Heading Adjustment algorithm.
+
+    This function will plot the original and revised fly heading along with the led angle.
+    """
+    fig, (ax1,ax2) = plt.subplots(2,1,figsize=(36,18))
+    split_idx = []
+    split_idx = (np.where(np.abs(np.diff(df_revised['Adjusted Angle']))>179)[0]+1)
+    print(split_idx)
+    sub_idx_groups = []
+    if len(split_idx>0):
+        for count,i in enumerate(split_idx):
+            if count == 0:
+                sub_idx_groups.append([0,i])
+            elif count == len(split_idx) -1:
+                sub_idx_groups.append([i,len(df_revised['Adjusted Angle'])])
+            else:
+                sub_idx_groups.append([i,split_idx[count+1]])
+        for group_idx in range(1,len(sub_idx_groups)):
+            print(split_idx[group_idx-1],split_idx[group_idx])
+        # Now what will be done is to rest this data so that it can just be directly plotted by filling in the proper interpolated value..
+        # f = interpolate.interp1d(df_male_dat['delta-t'], df_male_dat['fly heading'], kind = 'cubic')
+        ## basically doubled the number of points...\n",
+        # xnew = np.linspace(df_male_dat['delta-t'].loc[0],df_male_dat['delta-t'].loc[len(df_male_dat['delta-t'])-1],20000)
+        # ynew = f(xnew)   # use interpolation function returned by `interp1d`\n",
+    else:
+        # f = interpolate.interp1d(df_fem_loop['delta-t'],df_fem_loop['fly heading'], kind='cubic')
+
+
+        sub_idx_groups = [[0,len(df_revised['Adjusted Angle'])]]
+
+    for k_sub in range(len(sub_idx_groups)):
+        start_k = sub_idx_groups[k_sub][0]
+        end_k = sub_idx_groups[k_sub][1]
+        # Original Plot
+        ax1.plot(df_orig['delta-t'][start_k:end_k],df_orig['fly heading'][start_k:end_k],color = 'k',linewidth = .5, label = 'Original Trace')
+        ax1.plot(df_orig['delta-t'][start_k:end_k],df_orig['led angle'][start_k:end_k],color = 'goldenrod', linewidth = 1, label = 'LED')
+
+
+        # Revised Plot
+        ax2.plot(df_revised['delta-t'][start_k:end_k],df_revised['Adjusted Angle'][start_k:end_k],color = 'k', linewidth = .8, label = 'Adjusted Heading')
+        ax2.plot(df_revised['delta-t'][start_k:end_k],df_revised['led angle'][start_k:end_k],color = 'goldenrod', linewidth = 1, label = 'LED')
+    ax1.set_title("Fly Trace Original")
+    ax1.set_xlabel("time")
+    ax1.set_ylabel("Heading/LED Angle")
+    ax1.set_yticks(np.arange(-180,181,90))
+    ax1.legend(loc = "lower right")
+    ax2.set_title("Fly Trace Adjusted")
+    ax2.set_xlabel("time")
+    ax2.set_ylabel("Heading/LED Angle")
+    ax2.set_yticks(np.arange(-180,181,90))
+    ax2.legend(loc = "lower right")
+
+def wrapping_fix_old(data,subgroups,plot=True):
     """
     Parameters:
         - data:
